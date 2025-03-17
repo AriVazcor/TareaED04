4f53b88f8b38108c703606a43d239f64ccf82975
AriVazcor <arivazcor@gmail.com>
2025-03-17 22:15:11 +0000

    Primer commit

---
+++ src/kata3.java
@@ -0,0 +1,29 @@
+import java.lang.StringBuilder;
+
+class Solution {
+    /**
+     * Sustitución de los caracteres "-" por "_"
+     * @param s
+     * @return String del texto con los cambios realizados
+     */
+    public static String toCamelCase(String s) {
+        StringBuilder resultado = new StringBuilder();
+        boolean siguienteMayus = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char charActual = s.charAt(i);
+
+            if (charActual == '-' || charActual == '_') {
+                siguienteMayus = true;
+            } else {
+                if (siguienteMayus) {
+                    resultado.append(Character.toUpperCase(charActual));
+                    siguienteMayus = false;
+                } else {
+                    resultado.append(charActual);
+                }
+            }
+        }
+        return resultado.toString();
+    }
+}
